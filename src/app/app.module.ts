beaaac7ca2326e14b09813279a8455bb71ff0335
Shivakumar M <shivakumar.m2@mindtree.com>
2021-01-13 13:46:20 +0530

    push notification implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { UserModule } from './user/user.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { UserModule } from './user/user.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    UserModule
+    UserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user/adduser/adduser.component.ts
+++ src/app/user/adduser/adduser.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SwPush } from '@angular/service-worker';
 import { UserserviceService } from '../userservice.service';
 
 @Component({
@@ -11,7 +12,7 @@ export class AdduserComponent implements OnInit {
 
   userForm: FormGroup
 
-  constructor(private formBuilder: FormBuilder, private userService: UserserviceService) {
+  constructor(private swPush: SwPush, private formBuilder: FormBuilder, private userService: UserserviceService) {
     this.userForm = formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       age: ['', [Validators.required, Validators.min(18), Validators.max(60)]]
@@ -29,6 +30,25 @@ export class AdduserComponent implements OnInit {
     }
     this.userService.createUser(user);
     console.log(user)
+    this.notifyMe()    
+  }
+
+  notifyMe() {
+    if (!("Notification" in window)) {
+      console.log("This browser does not support desktop notification");
+    }
+  
+    else if (Notification.permission === "granted") {
+      var notification = new Notification("User Added Successfully");
+    }
+  
+    else if (Notification.permission !== 'denied') {
+      Notification.requestPermission(function (permission) {
+        if (permission === "granted") {
+          var notification = new Notification("Hi there!");
+        }
+      });
+    }
   }
 
 }
